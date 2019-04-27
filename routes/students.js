ab7a3f010db32cd1dfc97cb0debd6d8543f1e6b3
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-04-27 19:05:28 +0300

    now all students on /api route

--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const options = {
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'vkr_db',
+  },
+};
+
+const knex = require('knex')(options);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Express RESTful API');
+  knex
+    .from('students')
+    .select('*')
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    })
+    .finally(() => {
+      knex.destroy();
+    });
 });
 
 module.exports = router;
